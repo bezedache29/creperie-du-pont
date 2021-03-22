460fdce0589107bbba673d25a7ca67c670a739e6
bezedache29 <bezedache29@gmail.com>
2021-03-22 12:30:43 +0100

    Mise en place de vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,25 @@
 require('./bootstrap');
 import { createApp } from 'vue';
 import App from './components/App.vue'
-createApp(App).mount("#app")
+
+import store from './store'
+// import Vuex from 'vuex'
+
+// Vue.use(Vuex)
+
+// const store = new Vuex.Store({
+//   state: {
+    
+//   },
+//   mutations: {
+    
+//   },
+//   actions: {
+
+//   }
+// })
+
+const app = createApp(App)
+
+app.mount("#app")
+app.use(store)
