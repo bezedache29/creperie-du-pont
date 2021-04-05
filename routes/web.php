93d6183828ff35849b02fbc9874ad991e7652f23
bezedache29 <bezedache29@gmail.com>
2021-04-05 12:28:29 +0200

    Problème 404 au refresh résolu dans web.php

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController@index');
 
+Route::get('/{any}', 'HomeController@index')->where('any', '.*');
+
 // Route::get('/contact', 'HomeController@index');
