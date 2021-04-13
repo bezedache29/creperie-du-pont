1c0003cb0d56159a2ec4d342aca8b7bceda21138
bezedache29 <bezedache29@gmail.com>
2021-04-13 21:06:44 +0200

    Installation et mise en place de AOS

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,7 @@ import App from './components/App.vue'
 import router from './router/index'
 import store from './store'
 import 'animate.css'
+import 'aos/dist/aos.css'
 
 const app = createApp(App)
 
@@ -11,7 +12,5 @@ const app = createApp(App)
 app.use(router)
 
 app.use(store)
-
-
 
 app.mount("#app")
--- resources/js/views/presentation/RestaurantApp.vue
+++ resources/js/views/presentation/RestaurantApp.vue
@@ -13,17 +13,17 @@
 
     <!-- Restaurant description -->
     <div class="restaurant__section">
-      <div class="restaurant__section__w50 restaurant__section__w50--left">
+      <div class="restaurant__section__w50 restaurant__section__w50--left" data-aos="fade-right">
         <p class="restaurant__section__w50__desc">Vous ne pouvez pas la rater, avec ses beaux volets rouges. Vielles pierres, bois et murs blancs rendent l'intérieur châlereux, à l'image de notre équipe. Nous mettons en place une terrasse, ouverte l'été, qui vous permet d'admirer l'Aber.</p>
       </div>
-      <div class="restaurant__section__w50 restaurant__section__w50--right">
+      <div class="restaurant__section__w50 restaurant__section__w50--right" data-aos="fade-left">
         <img src="../../assets/images/restaurant/salle.jpg" alt="Salle restaurant">
       </div>
     </div>
 
     <!-- Restaurant galettes -->
     <div class="restaurant__section">
-      <div class="card">
+      <div class="card" data-aos="fade-right">
         <ul class="card__images">
           <li class="card__images__image">
             <img src="../../assets/images/restaurant/carte/galette.jpg" class="card__images__image__img" alt="photo galette">
@@ -45,13 +45,13 @@
           </li>
         </ul>
       </div>
-      <div class="restaurant__section__w50 restaurant__section__w50--solo">
+      <div class="restaurant__section__w50 restaurant__section__w50--solo" data-aos="fade-left">
         <p class="restaurant__section__w50__desc">Notre chef vous propose toute une gamme de galettes, ainsi que des salades, des pommes de terre cuisinées (les fameuses Patatez), des omelettes, des pizzas et des burgers, de quoi contenter tout le monde. En ce qui concerne les desserts, vous aurez l'embarras du choix entre les crêpes au froment et les coupes de glace.</p>
       </div>
     </div>
 
     <!-- Restaurant Enfants -->
-    <div class="restaurant__long-section">
+    <div class="restaurant__long-section" data-aos="flip-up">
       <p class="restaurant__long-section__description">La Crêperie du Pont veille à satisfaire aussi les petits, avec un menu adapté aux enfants et un coin étudié pour eux.</p>
       <div class="restaurant__long-section__image">
         <img src="../../assets/images/restaurant/enfants.jpg" alt="espace enfants">
@@ -59,7 +59,7 @@
     </div>
 
     <!-- Restaurant Concert -->
-    <div class="restaurant__long-section">
+    <div class="restaurant__long-section" data-aos="flip-down">
       <p class="restaurant__long-section__description">Enfin, la crêperie propose réguilièrement des diners-concerts, ce qui permet de passer une agréable soirée en musique.</p>
       <div class="restaurant__long-section__image restaurant__long-section__image--center">
         <img src="../../assets/images/restaurant/concert.jpg" alt="Diner concert">
@@ -85,6 +85,8 @@
 
 <script>
 
+import AOS from 'aos'
+
 import AubergesApp from '../../components/Auberges/AubergesApp'
 import PaymentMethods from '../../components/PaymentMethods/PaymentMethods'
 
@@ -94,6 +96,10 @@ export default {
   components: {
     PaymentMethods,
     AubergesApp
+  },
+
+  created() {
+    AOS.init()
   }
 
 }
